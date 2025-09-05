03b182f8f3cab74c4c3efaafe7815805c2277b9c
Nicholas Langford <dev@nlangford.au>
2025-09-05 21:06:02 +1000

    Add SPARE 4 problems/solutions; create template directory for contests (with macOS Makefile)

---
+++ contests/000-template/a.cpp
@@ -0,0 +1,46 @@
+// Author: scorpiontornado
+// Created: YYYY-MM-DD HH:MM
+// Source:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pq_min = priority_queue<T, vector<T>, greater<>>;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define repi(i, a, b) for (int i = (a); i <= (b); ++i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb emplace_back
+
+const int INF = 1e9;  // numeric_limits<int>::max();
+const ll LINF = 4e18;
+const double EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+void solve() {}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
