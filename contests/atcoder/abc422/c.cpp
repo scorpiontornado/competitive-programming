c7787e05671c146e476ca0f925b5ee10a900dd38
Nicholas Langford <dev@nlangford.au>
2025-09-08 23:46:00 +1000

    ABC422C - accepted!!!

--- contests/atcoder/abc422/c.cpp
+++ contests/atcoder/abc422/c.cpp
@@ -3,6 +3,28 @@
 // Source: https://atcoder.jp/contests/abc422/tasks/abc422_c
 
 // 11:33pm, testing. Not 100% convinced, but it's worth a shot...
+// 2025-09-08 10:28pm, debugging.. how tf -1? For 100 0 0. Also overcounting for
+//  sample 3/4, outputting 2 -1 1666666 60 1. 11:15pm, go fr!
+// 11:27pm, fixed? Remove debug prints & submit!
+// 11:31pm, AC!!!! Let's goooooooooo!!! Satisfying af, didn't read editorial.
+
+// What worked well?
+//  - Was stuck reasoning abt correctness of my o.g. alg & was WA'ing, so I...
+//      - Re-read the Q multiple times & started again from a fresh slate
+//      - Visualised it differently - allocating beads from piles A, B, C to
+//        piles ABC, AAC, and ACC
+//      - Tried to only use things I could be confident about their correctness:
+//          1. Take as many ABCs as possible (greedy stays ahead, in a way? Only
+//             way to use the B's, and the alternative is taking an A/C that
+//             could be used elsewhere)
+//          2. Was quite confident in my ability to manually solve cases w equal
+//             A's & C's, alternate. Rough proof in ob 2025-09-07 daily note(?)
+//          3. Was quite confident in my ability to reduce any case to that
+//             specific one (na == nc), as each AAC decreases delta = na - nc,
+//             and each ACC increases it. Just take one or the other until delta
+//             == 0 (or na == 0 or nc == 0)
+//  - Not 100% sure why I failed originally, prob smth to do with taking too
+//    many AAC's over ACC's (or vice versa if nc > na)? ... 11:45pm, try D?
 
 #pragma region template
 #include <bits/stdc++.h>
@@ -40,7 +62,11 @@ void solve() {
 
     // Take as many ABC as possible
     int ans = min({na, nb, nc});
-    na--, nb--, nc--;
+    // na--, nb--, nc--; //! BUG (caused the -1 for 100 0 0, made it 99 -1 -1)
+    na -= ans, nb -= ans, nc -= ans;
+
+    //! dbg
+    // cerr << "ans: " << ans << ", " << na << ' ' << nb << ' ' << nc << '\n';
 
     // Take AAC or ACC until delta == 0 (i.e. na == nc), or one of them is 0
     int delta = na - nc;
@@ -50,11 +76,17 @@ void solve() {
         ans += aac;
         na -= aac * 2;
         nc -= aac;
+
+        // cerr << "aac: " << aac << ", ans: " << ans << ", " << na << ' ' << nb
+        //      << ' ' << nc << '\n';  //! dbg
     } else if (delta < 0) {  // more C's
         int acc = min({-delta, na, nc / 2});
         ans += acc;
         na -= acc;
         nc -= acc * 2;
+
+        // cerr << "acc: " << acc << ", ans: " << ans << ", " << na << ' ' << nb
+        //      << ' ' << nc << '\n';  //! dbg
     }
 
     assert(na == nc || na == 0 || nc == 0);
@@ -63,6 +95,8 @@ void solve() {
     if (na > 0 && nc > 0) {
         // Take complete groups of 3 (implicit floor), valid because na == nc
         ans += (na + nc) / 3;
+        // cerr << "ans: " << ans << ", " << na << ' ' << nb << ' ' << nc
+        //      << '\n';  //! dbg
     }
 
     cout << ans << '\n';
