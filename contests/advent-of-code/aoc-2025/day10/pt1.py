e35b840290d6c2d17f86a953b3521d1eab6431a4
Nicholas Langford <dev@nlangford.au>
2025-12-11 00:49:12 +1100

    Clean up AoC 2025 day10 for Optiver interns highest code quality submission

--- contests/advent-of-code/aoc-2025/day10/pt1.py
+++ contests/advent-of-code/aoc-2025/day10/pt1.py
@@ -1,3 +1,4 @@
+import math
 import logging
 from functools import cache
 
@@ -8,8 +9,9 @@ ON = "#"
 
 
 # Memoise function calls with cache to avoid repeated calculation (DP)
+# (tuples are used rather than lists because they're hashable)
 @cache
-def min_presses(cur: int, desired: int, buttons: tuple[int, ...]) -> int | None:
+def min_presses(cur: int, desired: int, buttons: tuple[int, ...]) -> int | float:
     """Given a bitmask 'cur' representing the current state of lights
     (0 for off, 1 for on), a bitmask representing the desired state, and
     a list of bitmasks of buttons (which lights to toggle), represents
@@ -18,16 +20,13 @@ def min_presses(cur: int, desired: int, buttons: tuple[int, ...]) -> int | None:
     if cur == desired:
         return 0
 
-    best = None
+    best = math.inf
     for i, mask in enumerate(buttons):
-        # Never press same button twice, remove to avoid exceeding recursion depth
+        # Never press same button twice, remove to avoid exceeding recursion
+        # depth (state is small enough that it's fine, computation-wise)
         new_buttons = buttons[:i] + buttons[i + 1 :]
-        n_presses = min_presses(cur ^ mask, desired, new_buttons)
-
-        if n_presses is None:
-            continue
-        elif best is None or n_presses + 1 < best:
-            best = n_presses + 1
+        n_presses = min_presses(cur ^ mask, desired, new_buttons) + 1
+        best = min(best, n_presses)
 
     return best
 
@@ -59,8 +58,7 @@ for machine in machines:
 
     logging.debug([bin(b) for b in buttons])
 
-    n_presses = min_presses(0, desired, tuple(buttons))  # (tuples are hashable)
-    assert n_presses is not None
+    n_presses = min_presses(0, desired, tuple(buttons))
     total += n_presses
 
 print(total)
--- contests/advent-of-code/aoc-2025/day10/pt2.py
+++ contests/advent-of-code/aoc-2025/day10/pt2.py
@@ -1,12 +1,4 @@
-import logging
-from functools import cache
 import pulp
-
-# logging.basicConfig(level=logging.DEBUG)
-
-OFF = "."
-ON = "#"
-
 
 with open("input.txt") as f:
     machines = [l.strip("\n") for l in f.readlines()]
@@ -22,30 +14,35 @@ for i, machine in enumerate(machines):
     buttons = tokens[1:-1]
     sums = [[] for _ in range(len(desired))]
 
+    # Use linear programming to minimise the number of button presses needed to
+    # make the numeric counters satisfy the desired joltage requirement, where
+    # a given button press increments a particular set of counters.
+    # -> the variables are the number of times each button is pressed
+    # -> must minimise the sum of the variables (total num. of button presses)
+    # -> subject to the constraints that for each counter, the resulting
+    #    joltage must sum to the desired value
     prob = pulp.LpProblem("problem", pulp.LpMinimize)
     variables = [
         pulp.LpVariable(f"x_{i}", lowBound=0, cat="Integer")
         for i in range(len(buttons))
     ]
+
+    # Want to minimise the sum of the variables
     prob += pulp.lpSum(variables)
 
-    for i, button in enumerate(buttons):
-        for j in button[1:-1].split(","):
-            sums[int(j)].append(variables[i])
+    for button_idx, button in enumerate(buttons):
+        # Add the counters this button increments to the corresponding sums
+        counters = map(int, button[1:-1].split(","))
+        for counter_idx in counters:
+            sums[counter_idx].append(variables[button_idx])
 
     for idx, s in enumerate(sums):
-        print(idx, s, desired[idx])
+        # Counter "idx" must have a joltage equal to the desired value after
+        # all button presses for the solution to be valid
         prob += pulp.lpSum(s) == desired[idx]
 
-    cur = tuple(0 for _ in range(len(desired)))
-
     prob.solve(pulp.PULP_CBC_CMD(msg=False))
-
-    # total += int(pulp.value(prob.variables))
-    # TODO: fix?
-
-    print([var.varValue for var in variables])
     total += sum(var.varValue for var in variables)
-    print(total)
 
-print(total)
+print()
+print(f"{total=}")
